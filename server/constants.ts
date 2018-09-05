d92d02924c19495190b092ee0b3874cec61d9503
TerrySu <2684759855@qq.com>
2018-09-05 23:38:31 +0800

    Intialize

---
+++ server/constants.ts
@@ -0,0 +1,15 @@
+import * as PATH from 'path'
+const { resolve } = PATH
+
+
+export const ROOT = resolve( __dirname, '../' )
+export const SERVER = resolve( ROOT, 'server' )
+export const SRC = resolve( ROOT, 'src' )
+
+export const ENTRY = resolve( SRC, 'entry.tsx' )
+export const OUTPUT = resolve( ROOT, 'output' )
+
+export const OUTPUT_FILE_NAME = 'bundle.js'
+
+export const ENTRY_INDEX_HTML = resolve( SRC, 'index.html' )
+export const OUTPUT_INDEX_HTML = resolve( SRC, 'index.html' )
---
+++ webpack.config.ts
@@ -0,0 +1,58 @@
+import * as PATH from 'path'
+const { resolve } = PATH
+const webpack = require( 'webpack' )
+const CopyWebpackPlugin = require( "copy-webpack-plugin" )
+const CleanWebpackPlugin = require( "clean-webpack-plugin" )
+import { __DEV__ } from "./server/global"
+import { OUTPUT_FILE_NAME, ENTRY, OUTPUT, ENTRY_INDEX_HTML, OUTPUT_INDEX_HTML } from './server/constants';
+
+
+
+
+const webpackClientConfig = {
+  mode  : __DEV__ ? 'development' : 'production',
+  entry : {
+    [ OUTPUT_FILE_NAME ]: [ 
+      ENTRY,
+      `webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000`,   
+    ]
+  },
+  output: {
+    path      : OUTPUT,
+    filename  : '[name]',
+    publicPath: '/'
+  },
+  devtool: __DEV__ ? 'source-map' : false,
+  module : {
+    rules: [
+      {
+        test: /\.ts|\.tsx$/,
+        use : {
+          loader : 'ts-loader',
+        },
+        exclude: /node_modules/
+      },
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin( [
+      {
+        from: ENTRY_INDEX_HTML,
+        to  : OUTPUT_INDEX_HTML
+      }
+    ],
+   ),
+
+   new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+
+export default webpackClientConfig
